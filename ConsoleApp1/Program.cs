2500154bb2da58879365aff378c303431ee266db
Airiel Altemara <vsokord@gmail.com>
2024-05-09 18:25:58 -0700

    Csharp Acadamy test files initial upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random dice = new Random();
+int roll0 = dice.Next(50, 101);
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+Console.WriteLine(roll0);
+Console.WriteLine(roll1);
+Console.WriteLine(roll2);
+
+int number = 7;
+string text = "seven";
+
+Console.WriteLine(number); // 7
+Console.WriteLine(); // does nothing
+Console.WriteLine(text); // seven
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
