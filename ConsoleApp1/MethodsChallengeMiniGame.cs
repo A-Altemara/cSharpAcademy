37d8f0db58413e2b5a48296552babfca52b6173d
Airiel Altemara <vsokord@gmail.com>
2024-06-20 12:48:29 -0700

    Methods challenge MiniGame complete

--- ConsoleApp1/MethodsChallengeMiniGame.cs
+++ ConsoleApp1/MethodsChallengeMiniGame.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1;
 - Code to determine if the food consumed should increase player movement.
 - Code to increase movement speed.
 - Code to redisplay the food after it's consumed by the player.
-- Code to terminate execution if an unsupported key is entered.
+- Code to terminate execution if an unsupported key is entered. Done.
 - Code to terminate execution if the terminal was resized.
 */
 public static class MethodsChallengeMiniGame
@@ -18,10 +18,11 @@ public static class MethodsChallengeMiniGame
         int height = Console.WindowHeight - 1;
         int width = Console.WindowWidth - 5;
         bool shouldExit = false;
+        bool foodConsumed = false;
 
         // Console position of the player
-         int playerX = 0;
-         int playerY = 0;
+         int playerX = width / 2;
+         int playerY = height / 2;
 
         // Console position of the food
          int foodX = 0;
@@ -31,32 +32,39 @@ public static class MethodsChallengeMiniGame
          string[] states =
         {
             "('-')", "(^-^)", "(X_X)"
-        }
-        ;
+        };
          string[] foods =
         {
             "@@@@@", "$$$$$", "#####"
-        }
-        ;
+        };
 
         // Current player string displayed in the Console
-         string player =
-        states[0];
+         string player = states[0];
 
         // Index of the current food
-         int food =
-        0;
-         
+         int food = 0;
+
+         Console.WriteLine("Use Arrow Keys or keys A S W D to move and consume food. ");
          InitializeGame();
         while (!shouldExit)
         {
-            Move();
-        }
+            if (TerminalResized())
+            {
+                Console.Clear();
+                Console.WriteLine("Console was resized. program exiting.");
+                shouldExit = true;
+            }
 
+            int playerspeed = CheckSpeedyPlayerState();
+            CheckFrozenPlayerState();
+            Move(playerspeed);
+            IsFoodEaten(playerX, playerY, foodX, foodY);
+        }
+        
         // Returns true if the Terminal was resized 
         bool TerminalResized()
         {
-            return height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
+            return height != (Console.WindowHeight - 1) || width != (Console.WindowWidth - 5);
         }
 
         // Displays random food at a random location
@@ -67,7 +75,23 @@ public static class MethodsChallengeMiniGame
 
             // Update food position to a random location
             foodX = random.Next(0, width - player.Length);
-            foodY = random.Next(0, height - 1);
+            if (foodX >= playerX)
+            {
+                foodX += player.Length;
+            }
+
+            // int playerSpeed = CheckSpeedyPlayerState();
+            // do
+            // {
+            //     foodY = random.Next(0, height - 1);
+            // } while (foodY % playerSpeed != playerY % playerSpeed);
+            //
+            // foodY = random.Next(0, (height - 1) / playerSpeed) * playerSpeed + (playerY % playerSpeed);
+            
+            if (foodY >= playerY)
+            {
+                foodY++;
+            }
 
             // Display the food at the location
             Console.SetCursorPosition(foodX, foodY);
@@ -90,7 +114,7 @@ public static class MethodsChallengeMiniGame
         }
 
         // Reads directional input from the Console and moves the player
-        void Move()
+        void Move(int speed = 1)
         {
             int lastX = playerX;
             int lastY = playerY;
@@ -98,24 +122,25 @@ public static class MethodsChallengeMiniGame
             switch (Console.ReadKey(true).Key)
             {
                 case ConsoleKey.UpArrow:
+                case ConsoleKey.W:
                     playerY--;
                     break;
                 case ConsoleKey.DownArrow:
+                case ConsoleKey.S:
                     playerY++;
                     break;
                 case ConsoleKey.LeftArrow:
-                    playerX--;
+                case ConsoleKey.A:
+                    playerX = lastX - speed;
                     break;
                 case ConsoleKey.RightArrow:
-                    playerX++;
+                case ConsoleKey.D:
+                    playerX = lastX + speed;
                     break;
                 case ConsoleKey.Escape:
-                    shouldExit = true;
-                    break;
                 default:
                     shouldExit = true;
                     break;
-                
             }
 
             // Clear the characters at the previous position
@@ -133,15 +158,56 @@ public static class MethodsChallengeMiniGame
             Console.SetCursorPosition(playerX, playerY);
             Console.Write(player);
         }
+        
+        // checks to see if player has consumed food
+        void IsFoodEaten(int playerXLocation, int playerYLocation, int foodXLocation, int foodYLocation)
+        {
+            if (playerXLocation == foodXLocation && playerYLocation == foodYLocation)
+            {
+                ChangePlayer();
+                ShowFood();
+            }
+            // return foodConsumed;
+        }
 
         // Clears the console, displays the food and player
         void InitializeGame()
         {
             Console.Clear();
             ShowFood();
-            Console.SetCursorPosition(0, 0);
+            Console.SetCursorPosition(playerX, playerY);
             Console.Write(player);
+        }
 
+        void PrintUpdate()
+        {
+            // var foo = Console.GetCursorPosition();
+            // var l = foo.Left;
+            // var t = foo.Top;
+            var (left, top) = Console.GetCursorPosition();
+            Console.SetCursorPosition(0,0);
+            Console.Write($"Food: {food} FoodX: {foodX} FoodY: {foodY}");
+            Console.SetCursorPosition(left, top);
+        }
+
+        bool CheckFrozenPlayerState()
+        {
+            if (player == states[2])
+            {
+                FreezePlayer();
+                return true;
+            }
+
+            return false;
+        }
+        int CheckSpeedyPlayerState()
+        {
+            if (player == states[1])
+            {
+                return 3;
+            }
+
+            return 1;
         }
     }
 }
